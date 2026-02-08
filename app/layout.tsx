ba7c0a6a517792ed7c3e48cf00534a7e5ff99bcb
ventelbell-code <ventelbell@gmail.com>
2026-02-08 14:10:28 +0100

    fix layout render children

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import "./globals.css"
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
     </html>
   )
 }
